0e078fd58dbdce77deb9d1172f0f7c626b07bbfd
Ahmed Fasih <wuzzyview@gmail.com>
2015-07-02 01:42:27 -0400

    Moved review to its own dir

--- public/build.sh
+++ public/build.sh
@@ -3,8 +3,7 @@ cp deckbuilder.html index.html
 sed -i.bak -E 's/deckbuilder.js/deckbuilder.compat.js/' index.html
 babel deckbuilder.js > deckbuilder.compat.js
 
-mkdir -p review
-cp review.html review/index.html
+cp review/review.html review/index.html
 sed -i.bak -E 's/review.js/review.compat.js/' review/index.html
-babel review.js > review/review.compat.js
+babel review/review.js > review/review.compat.js
 
